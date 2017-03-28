a80ffc33cc57f9528f7b3989a12d185442ec8078
Maribel <mar.garcia1190@gmail.com>
2017-03-28 09:43:37 -0600

    Version 2

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Esta es una guía para correr el script **send_answers.py**     
 
 1.- Clonar repo en tu máquina local  
-2.- Entrar a tu entorno virtual desde una nueva terminal  
-3.- Ejecutar el siguiente comando: `python ./send_answers.py`
+2.- Abrir una nueva terminal  
+3.- Ejecutar el siguiente comando: `python ./send_answers.py`  
+4.- Comprobar en la url `http://localhost:9200/beeva/mary/1?pretty`
--- send_answers.py
+++ send_answers.py
@@ -2,13 +2,14 @@
 import json
 import requests
 
-url = 'http://localhost:80/mary'
+url = 'http://localhost:9200/beeva/mary/1?pretty'
 
 #Abrir y leer json
 with open ('/home/administradorcito/introduction-quiz/introduction_quiz.json') as qz:
      qui = json.load(qz)
 print(qui)
 
-#read = requests.post(url, qui=json.dumps(qui))
+read = requests.post(url, json = qui)
+print(read.text)
 
 #curl -i -X POST --data @introduction_quiz.json http://localhost:80
